ac7cab136ff9c1a478f4c405ba16feccdb9b0d3a
daniir <danrodas03@gmail.com>
2024-02-22 16:04:19 -0500

    main.go

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/daniir/go_api/database"
+	"github.com/daniir/go_api/models"
+	"github.com/daniir/go_api/routes"
+	"github.com/daniir/go_api/settings"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	s, err := settings.New()
+	if err != nil {
+		panic("Error")
+	}
+
+	db, _ := database.DbConnection(s)
+	todo := routes.New(*db)
+
+	db.AutoMigrate(&models.Todo{})
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", todo.GetTodos).Methods("GET")
+	r.HandleFunc("/{id}", todo.GetTodo).Methods("GET")
+	r.HandleFunc("/", todo.CreateNewTodo).Methods("POST")
+	r.HandleFunc("/{id}", todo.UpdateTodoStatus).Methods("PUT")
+	r.HandleFunc("/{id}", todo.DeleteTodo).Methods("DELETE")
+
+	port := fmt.Sprintf(":%d", s.Port)
+
+	http.ListenAndServe(port, r)
+	fmt.Printf("Server al localhost:%d\n", s.Port)
+}
